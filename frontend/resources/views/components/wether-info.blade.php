b7103411895c26d1cd6f32a1665490acb54abef5
robinmahz <robinmaharjan25@gmail.com>
2025-01-30 07:28:42 +0545

    removed frontend

--- frontend/resources/views/components/wether-info.blade.php
+++
@@ -1,8 +0,0 @@
-<!-- Weather Information -->
-<div id="weatherInfo"
-    class="mt-6 p-6 bg-white rounded-xl shadow-lg transition-all duration-300 ease-in-out transform hover:scale-105">
-    <h4 class="text-xl font-semibold text-primary">🌤️ Weather Information:</h4>
-    <p><strong>Temperature:</strong> <span id="temp">N/A</span> °C</p>
-    <p><strong>Weather:</strong> <span id="weatherDesc">N/A</span></p>
-    <p><strong>Humidity:</strong> <span id="humidity">N/A</span>%</p>
-</div>
--- frontend/resources/views/mail/newsletter-mail.blade.php
+++
@@ -1,1 +0,0 @@
-thank you for subscribing to our newsletter
