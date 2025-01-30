c1003fec414e7fd13efa8bc4866c7399fe484bfe
Paracetamol77 <joshikamal134@gmail.com>
2025-01-30 21:48:49 +0545

    dark css removed

--- frontend/resources/views/components/faq.blade.php
+++ frontend/resources/views/components/faq.blade.php
@@ -1,16 +1,16 @@
 <section class="container mt-8">
     <div x-data="{ selectedAccordionItem: 'one' }"
-        class="w-full divide-y divide-neutral-300 overflow-hidden rounded-md border border-neutral-300 bg-neutral-50/40 text-neutral-600 dark:divide-neutral-700 dark:border-neutral-700 dark:bg-neutral-900/50 dark:text-neutral-300">
-        <div class="divide-y divide-neutral-300 dark:divide-neutral-700">
+        class="w-full overflow-hidden border divide-y rounded-md divide-neutral-300 border-neutral-300 bg-neutral-50/40 text-neutral-600">
+        <div class="divide-y divide-neutral-300">
             <button id="controlsAccordionItemOne" type="button"
-                class="flex w-full items-center justify-between gap-4 bg-neutral-50 p-4 text-left underline-offset-2 hover:bg-neutral-50/75 focus-visible:bg-neutral-50/75 focus-visible:underline focus-visible:outline-none dark:bg-neutral-900 dark:hover:bg-neutral-900/75 dark:focus-visible:bg-neutral-900/75"
+                class="flex items-center justify-between w-full gap-4 p-4 text-left bg-neutral-50 underline-offset-2 hover:bg-neutral-50/75 focus-visible:bg-neutral-50/75 focus-visible:underline focus-visible:outline-none"
                 aria-controls="accordionItemOne" @click="selectedAccordionItem = 'one'"
-                :class="selectedAccordionItem === 'one' ? 'text-onSurfaceStrong dark:text-onSurfaceDarkStrong font-bold' :
-                    'text-onSurface dark:text-onSurfaceDark font-medium'"
+                :class="selectedAccordionItem === 'one' ? 'text-onSurfaceStrong ' :
+                    'text-onSurface '"
                 :aria-expanded="selectedAccordionItem === 'one' ? 'true' : 'false'">
                 What products does Agrimandu offer?
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke-width="2"
-                    stroke="currentColor" class="size-5 shrink-0 transition" aria-hidden="true"
+                    stroke="currentColor" class="transition size-5 shrink-0" aria-hidden="true"
                     :class="selectedAccordionItem === 'one' ? 'rotate-180' : ''">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                 </svg>
@@ -23,16 +23,16 @@
                 </div>
             </div>
         </div>
-        <div class="divide-y divide-neutral-300 dark:divide-neutral-700">
+        <div class="divide-y divide-neutral-300">
             <button id="controlsAccordionItemTwo" type="button"
-                class="flex w-full items-center justify-between gap-4 bg-neutral-50 p-4 text-left underline-offset-2 hover:bg-neutral-50/75 focus-visible:bg-neutral-50/75 focus-visible:underline focus-visible:outline-none dark:bg-neutral-900 dark:hover:bg-neutral-900/75 dark:focus-visible:bg-neutral-900/75"
+                class="flex items-center justify-between w-full gap-4 p-4 text-left bg-neutral-50 underline-offset-2 hover:bg-neutral-50/75 focus-visible:bg-neutral-50/75 focus-visible:underline focus-visible:outline-none"
                 aria-controls="accordionItemTwo" @click="selectedAccordionItem = 'two'"
-                :class="selectedAccordionItem === 'two' ? 'text-onSurfaceStrong dark:text-onSurfaceDarkStrong font-bold' :
-                    'text-onSurface dark:text-onSurfaceDark font-medium'"
+                :class="selectedAccordionItem === 'two' ? 'text-onSurfaceStrong ' :
+                    'text-onSurface '"
                 :aria-expanded="selectedAccordionItem === 'two' ? 'true' : 'false'">
                 How does Agrimandu support rooftop farmers?
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke-width="2"
-                    stroke="currentColor" class="size-5 shrink-0 transition" aria-hidden="true"
+                    stroke="currentColor" class="transition size-5 shrink-0" aria-hidden="true"
                     :class="selectedAccordionItem === 'two' ? 'rotate-180' : ''">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                 </svg>
@@ -45,16 +45,16 @@
                 </div>
             </div>
         </div>
-        <div class="divide-y divide-neutral-300 dark:divide-neutral-700">
+        <div class="divide-y divide-neutral-300">
             <button id="controlsAccordionItemThree" type="button"
-                class="flex w-full items-center justify-between gap-4 bg-neutral-50 p-4 text-left underline-offset-2 hover:bg-neutral-50/75 focus-visible:bg-neutral-50/75 focus-visible:underline focus-visible:outline-none dark:bg-neutral-900 dark:hover:bg-neutral-900/75 dark:focus-visible:bg-neutral-900/75"
+                class="flex items-center justify-between w-full gap-4 p-4 text-left bg-neutral-50 underline-offset-2 hover:bg-neutral-50/75 focus-visible:bg-neutral-50/75 focus-visible:underline focus-visible:outline-none"
                 aria-controls="accordionItemThree" @click="selectedAccordionItem = 'three'"
-                :class="selectedAccordionItem === 'three' ? 'text-onSurfaceStrong dark:text-onSurfaceDarkStrong font-bold' :
-                    'text-onSurface dark:text-onSurfaceDark font-medium'"
+                :class="selectedAccordionItem === 'three' ? 'text-onSurfaceStrong ' :
+                    'text-onSurface '"
                 :aria-expanded="selectedAccordionItem === 'three' ? 'true' : 'false'">
                 How can Agrimandu help farmers increase productivity?
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke-width="2"
-                    stroke="currentColor" class="size-5 shrink-0 transition" aria-hidden="true"
+                    stroke="currentColor" class="transition size-5 shrink-0" aria-hidden="true"
                     :class="selectedAccordionItem === 'three' ? 'rotate-180' : ''">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                 </svg>
--- frontend/resources/views/components/navbar.blade.php
+++ frontend/resources/views/components/navbar.blade.php
@@ -1,14 +1,14 @@
-<nav class="bg-primary dark:bg-gray-900 fixed w-full top-0 start-0 border-b border-gray-200 dark:border-gray-600 z-50">
-    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+<nav class="fixed top-0 z-50 w-full border-b border-gray-200 bg-primary start-0 ">
+    <div class="flex flex-wrap items-center justify-between max-w-screen-xl p-4 mx-auto">
         <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
             <img src="{{ asset('images/AgrimanduLogo.png') }}" class="h-8" alt="AgriMandu Logo">
         </a>
-        <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-            <form class="hidden lg:flex flex-1 items-center justify-end" method="POST" action="/search">
+        <div class="flex space-x-3 md:order-2 md:space-x-0 rtl:space-x-reverse">
+            <form class="items-center justify-end flex-1 hidden lg:flex" method="POST" action="/search">
                 @csrf
-                <label for="default-search" class="mb-2 text-sm font-medium sr-only text-white">Search</label>
+                <label for="default-search" class="mb-2 text-sm font-medium text-white sr-only">Search</label>
                 <div class="relative">
-                    <div class="absolute inset-y-0 start-0 flex items-center ps-3 ">
+                    <div class="absolute inset-y-0 flex items-center start-0 ps-3 ">
                         <svg class="w-4 h-4 text-gray-400 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                             fill="none" viewBox="0 0 20 20">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -21,7 +21,7 @@
                 </div>
             </form>
             <button data-collapse-toggle="navbar-sticky" type="button"
-                class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                class="inline-flex items-center justify-center w-10 h-10 p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
                 aria-controls="navbar-sticky" aria-expanded="false">
                 <span class="sr-only">Open main menu</span>
                 <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
@@ -65,20 +65,20 @@
         @endphp
         <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
             <ul
-                class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-primary md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                class="flex flex-col p-4 mt-4 font-medium border border-gray-100 rounded-lg md:p-0 bg-primary md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
                 @foreach ($nav as $key => $item)
                     <li>
                         @if (isset($item['dropdown']))
                             <div class="relative group">
                                 <a href="#"
-                                    class=" py-2 px-3 rounded md:bg-transparent md:p-0 capitalize hover:text-accent duration-150 text-white flex gap-2">{{ $item['name'] }}
+                                    class="flex gap-2 px-3 py-2 text-white capitalize duration-150 rounded md:bg-transparent md:p-0 hover:text-accent">{{ $item['name'] }}
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                         stroke-width="1.5" stroke="currentColor" class="size-6">
                                         <path stroke-linecap="round" stroke-linejoin="round"
                                             d="m19.5 8.25-7.5 7.5-7.5-7.5" />
                                     </svg>
                                 </a>
-                                <ul class="absolute hidden group-hover:block bg-white shadow-lg rounded">
+                                <ul class="absolute hidden bg-white rounded shadow-lg group-hover:block">
                                     @foreach ($item['dropdown'] as $subitem)
                                         <li><a href="/{{ $subitem['route'] }}"
                                                 class="block px-4 py-2 text-black hover:bg-gray-200">{{ $subitem['name'] }}</a>
